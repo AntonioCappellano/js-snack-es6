8929eeb23171bae18f757c19f7ff0e6c2dc954c3
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-16 17:03:39 +0100

    create a new array without goals scored

--- snack2/snack2.js
+++ snack2/snack2.js
@@ -13,7 +13,7 @@ const squadreDiCalcio = [
 const generateRandomNumber = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
-console.log(generateRandomNumber(0, 10));
+// console.log(generateRandomNumber(0, 10));
 
 // genero numeri random al posto degli 0
 
@@ -21,4 +21,20 @@ for (let squadra of squadreDiCalcio) {
   squadra.puntiFatti = generateRandomNumber(0, 10);
   squadra.falliSubiti = generateRandomNumber(0, 10);
 }
+// console.table(squadreDiCalcio);
+
+// creo un nuovo array solo con nomi e falli subiti
+
+let senzaPunti = [];
+
+for (let squadra of squadreDiCalcio) {
+  senzaPunti.push({
+    nome: squadra.nome,
+    falliSubiti: squadra.falliSubiti,
+  });
+}
+
+console.log("array con le squadre di calcio:");
 console.table(squadreDiCalcio);
+console.log("array senza i punti fatti:");
+console.table(senzaPunti);
