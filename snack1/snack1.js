302d751657afa5ab0aed886997b05a3d5141ef56
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-16 14:32:04 +0100

    make object array

---
+++ snack1/snack1.js
@@ -0,0 +1,8 @@
+// creo un array di oggetti
+const biciclette = [
+  { nome: "bici1", peso: 50 },
+  { nome: "bici2", peso: 100 },
+  { nome: "bici3", peso: 20 },
+  { nome: "bici4", peso: 40 },
+  { nome: "bici5", peso: 70 },
+];
