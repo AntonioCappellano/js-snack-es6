564ac2c684eae09e932d0f47c39464ee5ef133b0
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-16 14:50:33 +0100

    make object array of footbal teams

---
+++ snack2/snack2.js
@@ -0,0 +1,9 @@
+// creo un array di squadre di calcio
+
+const squadreDiCalcio = [
+  { nome: "squadra1", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "squadra2", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "squadra3", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "squadra4", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "squadra5", puntiFatti: 0, falliSubiti: 0 },
+];
